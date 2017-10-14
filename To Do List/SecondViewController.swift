e554b3a2d1665417208e6bdb2b90d3141b576b27
Gerson <gerson.lcc@hotmail.com>
2017-10-14 19:27:16 +0100

    Save Btn configured

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,23 +8,45 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var taskSaved: UILabel!
     
+    var listArray = [String] ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    // Saves the task inside an array and sends a message informing task was saved
     @IBAction func saveBtnPressed(_ sender: UIButton) {
+        
+        if let task = textField.text {
+            
+            listArray.append(task)
+            
+            taskSaved.text = "\(task) was saved!"
+            
+            taskSaved.isHidden = false
+            
+        }
+    }
+    // Closes keyboard when it is open and user taps outside the keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    // Closes keyboard when user taps the return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
     }
     
 }
